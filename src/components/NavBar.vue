ea426618333b06d918f438a0febfc8b49ce06a6f
Bognár László <blaszlo89@gmail.com>
2022-08-24 01:59:28 +0200

    Added component: NavBar

---
+++ src/components/NavBar.vue
@@ -0,0 +1,9 @@
+<template>
+    <nav class="navbar sticky-top bg-white">
+        <div class="container">
+            <a class="navbar-brand" href="">
+                <img alt="ingatlan.com" class="logo" src="@/assets/logo.svg" width="218"/>
+            </a>
+        </div>
+    </nav>
+</template>
